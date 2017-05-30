3889490f2f78145215555c56a407797e64557ed1
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2017-05-30 14:02:52 +0100

    Update Fizz Buzz with Antoine's idea.

--- src/main/java/com/evolutionnext/FizzBuzz.java
+++ src/main/java/com/evolutionnext/FizzBuzz.java
@@ -2,9 +2,9 @@ package com.evolutionnext;
 
 public class FizzBuzz {
     public static String translate(int num) {
-        if (num % 3 == 0 && num % 5 == 0) return "FizzBuzz";
-        if (num % 3 == 0) return "Fizz";
-        if (num % 5 == 0) return "Buzz";
-        return "" + num;
+        StringBuilder stringBuilder = new StringBuilder();
+        if (num % 3 == 0) stringBuilder.append("Fizz");
+        if (num % 5 == 0) stringBuilder.append("Buzz");
+        return (stringBuilder.length() == 0) ? "" + num : stringBuilder.toString();
     }
 }
