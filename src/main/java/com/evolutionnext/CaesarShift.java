97997fa395e48ad5024539912b10901a168c1a99
Mihai Bojin <mihai.bojin@gmail.com>
2017-05-30 10:44:19 +0100

    Functional solution for Caesar Shift
    > NOTE: needs Guava added as dependency in [pom.xml](/pom.xml)
    
    ```
           <dependency>
               <groupId>com.google.guava</groupId>
               <artifactId>guava</artifactId>
               <version>22.0</version>
           </dependency>
    ```

--- src/main/java/com/evolutionnext/CaesarShift.java
+++ src/main/java/com/evolutionnext/CaesarShift.java
@@ -1,35 +1,59 @@
 package com.evolutionnext;
 
+import com.google.common.base.Preconditions;
+
 import java.util.stream.Collectors;
 
+/**
+ * Caesar shift encoder class
+ * <p/>
+ * <p/> - Works for A-Z and a-z
+ * <p/> - Leaves all the other characters unmodified
+ *
+ * @author Mihai Bojin &lt;mbojin@salesforce.com&gt;
+ * @since 1.0
+ */
 public class CaesarShift {
-    private final int shift;
+    private final static int ALPHABET_SIZE = 26;
+    public final int shift;
 
     public CaesarShift(int shift) {
-        this.shift = shift % 26;
+        this.shift = shift;
     }
 
-    /*
-    private int convert(int c, int shift) {
-		if ((c < 'A' || c > 'Z') && (c < 'a' || c > 'z')) return c;
-		int offset = Character.isLowerCase(c) ? 'a' : 'A';
-		int charShifted = c + shift - offset;
-		int alphaLen = 'Z' - 'A' + 1;
-		return (charShifted % alphaLen + alphaLen) % alphaLen + offset;
-	}
+    public String encrypt(String message) {
+        Preconditions.checkNotNull(message);
+
+        return message.chars().boxed()
+
+                // lowercase
+                .map(c -> c - 97)
+                .map(this::encode)
+                .map(c -> c + 97)
+
+                // uppercase
+                .map(c -> c - 65)
+                .map(this::encode)
+                .map(c -> c + 65)
+
+                // convert back to Character
+                .map(c -> Character.toString((char)c.intValue()))
+
+                // collect back to String
+                .collect(Collectors.joining());
+    }
+
+    /**
+     * Encode if a-z or A-Z or return the input character
      */
-
-    private char encryptChar(char chr) {
-        char temp = (char) (chr + shift);
-        return (temp > 'z' || (temp > 'Z' && temp < 'a')) ? (char) (temp - 26) : temp;
+    private Integer encode(int inputChar) {
+        return shouldEncode(inputChar)?Math.floorMod(shift + inputChar, ALPHABET_SIZE):inputChar;
     }
 
-    public String encrypt(String string) {
-        if (string == null) throw
-                new NullPointerException("String cannot be null");
-        if (string.isEmpty()) return string;
-        return string.chars().boxed()
-                     .map(integer -> "" + encryptChar((char) integer.intValue()))
-                     .collect(Collectors.joining());
+    /**
+     * Returns true if the character is between positions 0-26
+     */
+    private static boolean shouldEncode(int inputChar) {
+        return (inputChar >= 0) && (inputChar <= ALPHABET_SIZE);
     }
 }
