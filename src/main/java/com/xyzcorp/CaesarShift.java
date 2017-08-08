4995124b7d4a16c152c41089a236f768730eee5c
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2017-08-08 17:07:40 +0530

    added Caesar Shift code

---
+++ src/main/java/com/xyzcorp/CaesarShift.java
@@ -0,0 +1,45 @@
+package com.xyzcorp;
+
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class CaesarShift {
+
+	private static final int ALPHA_LENGTH = 26;
+	private static final int SMALL_A = 97;
+	private static final int BIG_A = 65;
+	public static final String STRING_CANNOT_BE_NULL = "String cannot be null";
+	private final int shift;
+	
+	public CaesarShift(int shift) {
+		this.shift = shift;
+	}
+
+	private char shiftChar(char c, int actualShift) {
+		if (!Character.isLetter(c)) return c;
+		int preferredA = Character.isUpperCase(c) ? BIG_A : SMALL_A;
+		return (char)((c + (actualShift % ALPHA_LENGTH + ALPHA_LENGTH) 
+				                                     - preferredA) 
+				                                     % ALPHA_LENGTH + preferredA);
+	}
+	
+	private String shiftString(String string, int actualShift) {
+		Objects.requireNonNull(string, STRING_CANNOT_BE_NULL);
+		if (string.isEmpty()) return "";
+		return string
+				.chars()
+				.boxed()
+				.map(i -> "" + shiftChar((char) i.intValue(), actualShift))
+				.collect(Collectors.joining());
+	}
+	
+	public String encode(String string) {
+		return shiftString(string, shift);
+	}
+
+	public String decode(String string) {
+		return shiftString(string, -shift);
+	}
+}
---
+++ src/test/java/com/xyzcorp/CaesarShiftTest.java
@@ -0,0 +1,153 @@
+package com.xyzcorp;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.rules.ExpectedException;
+
+@Category(value = UnitTest.class)
+public class CaesarShiftTest {
+	// What are the easier inputs
+
+	@Test
+	public void testShiftOfEmptyStringAnd0() {
+		CaesarShift cs = new CaesarShift(0);
+		assertEquals("", cs.encode(""));
+	}
+
+	@Test
+	public void testShiftOfZeroCharStringOfA() {
+		CaesarShift cs = new CaesarShift(0);
+		assertThat(cs.encode("a")).isEqualTo("a");
+	}
+
+	@Test
+	public void testShiftOfOneCharStringOfA() {
+		CaesarShift cs = new CaesarShift(1);
+		assertThat(cs.encode("a")).isEqualTo("b");
+	}
+
+	@Test
+	public void testEncodeWithNullClassicForm() {
+		CaesarShift cs = new CaesarShift(1);
+		try {
+			cs.encode(null);
+			fail("This should not run");
+		} catch (NullPointerException e) {
+			assertThat(e).hasMessage(CaesarShift.STRING_CANNOT_BE_NULL);
+		}
+	}
+
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+
+	// GreenBar
+	@Test
+	public void testEncodeWithAJUnitRule() {
+		thrown.expect(NullPointerException.class);
+		thrown.expectMessage(CaesarShift.STRING_CANNOT_BE_NULL);
+		CaesarShift cs = new CaesarShift(1);
+		cs.encode(null);
+	}
+
+	// GreenBar
+	@Test
+	public void testEncodeWithANullUsingAssertJFunction() {
+		CaesarShift cs = new CaesarShift(1);
+		assertThatThrownBy(() -> cs.encode(null)).isInstanceOf(NullPointerException.class)
+				.hasMessage(CaesarShift.STRING_CANNOT_BE_NULL);
+	}
+
+	@Test
+	public void testShiftOfOneCharStringOfZ() {
+		CaesarShift cs = new CaesarShift(1);
+		assertThat(cs.encode("z")).isEqualTo("a");
+	}
+
+	@Test
+	public void testShiftOfTwoCharStringOfX() {
+		CaesarShift cs = new CaesarShift(2);
+		assertThat(cs.encode("y")).isEqualTo("a");
+	}
+
+	@Test
+	public void testShiftOf27CharStringOfZ() {
+		CaesarShift cs = new CaesarShift(27);
+		assertThat(cs.encode("z")).isEqualTo("a");
+	}
+
+	@Test
+	public void testShiftOfNeg2CharStringOfA() {
+		CaesarShift cs = new CaesarShift(-2);
+		assertThat(cs.encode("a")).isEqualTo("y");
+	}
+
+	@Test
+	public void testShiftOfNeg27CharStringOfA() {
+		CaesarShift cs = new CaesarShift(-27);
+		assertThat(cs.encode("a")).isEqualTo("z");
+	}
+
+	@Test
+	public void testShiftOfOneCharStringOfCapA() {
+		CaesarShift cs = new CaesarShift(1);
+		assertThat(cs.encode("A")).isEqualTo("B");
+	}
+
+	@Test
+	public void testShiftOf0WithWithStrangeChar() {
+		CaesarShift cs = new CaesarShift(0);
+		assertThat(cs.encode("~")).isEqualTo("~");
+	}
+
+	@Test
+	public void testDecodeShiftOfEmptyStringAnd0() {
+		CaesarShift cs = new CaesarShift(0);
+		assertEquals("", cs.decode(""));
+	}
+
+	@Test
+	public void testDecodeShiftOfZeroCharStringOfA() {
+		CaesarShift cs = new CaesarShift(0);
+		assertThat(cs.decode("a")).isEqualTo("a");
+	}
+
+	@Test
+	public void testDecodeShiftOfOneCharStringOfA() {
+		CaesarShift cs = new CaesarShift(1);
+		assertThat(cs.decode("a")).isEqualTo("z");
+	}
+
+	@Test
+	public void testEncodeShiftOfATwoLetterWordStringOfA() {
+		CaesarShift cs = new CaesarShift(1);
+		assertThat(cs.encode("ab")).isEqualTo("bc");
+	}
+
+	@Test
+	public void testDecodeShiftOfATwoLetterWordStringOfA() {
+		CaesarShift cs = new CaesarShift(1);
+		assertThat(cs.decode("ab")).isEqualTo("za");
+	}
+
+	// GreenBar
+	@Test
+	public void testHelloWorldEncode() {
+		CaesarShift cs = new CaesarShift(1);
+		assertThat(cs.encode("Hello World!")).isEqualTo("Ifmmp Xpsme!");
+	}
+
+	// GreenBar
+	@Test
+	public void testHelloWorldDecode() {
+		CaesarShift cs = new CaesarShift(1);
+		assertThat(cs.decode(cs.encode("Hello World!"))).isEqualTo("Hello World!");
+	}
+
+	// do we want to encode numbers, for now no, but we can
+}
