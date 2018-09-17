9cabe7672aa50db6f9744f7f4803436c112a76d4
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2018-09-17 08:21:40 -0600

    Reset for 9/17 class.

--- src/test/java/com/xyzcorp/cucumber/AccountWorld.java
+++
@@ -1,9 +0,0 @@
-package com.xyzcorp.cucumber;
-
-import com.xyzcorp.Account;
-import cucumber.runtime.java.guice.ScenarioScoped;
-
-@ScenarioScoped
-public class AccountWorld {
-    public Account account;
-}
