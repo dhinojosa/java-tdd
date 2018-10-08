af1101c591b2de9f644c8dd7ff811312b4378e6f
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2018-10-08 15:53:37 -0600

    Just added z + 1 test

--- src/main/java/com/xyzcorp/instructor/CaesarShift.java
+++ src/main/java/com/xyzcorp/instructor/CaesarShift.java
@@ -1,8 +1,19 @@
 package com.xyzcorp.instructor;
 
 public class CaesarShift {
+
+    public static final int ALPHA_SIZE = 26;
+    public static final char SMALL_A = 'a';
+
     public static String encode(String string, int shift) {
-        if (string.isEmpty()) return string;
-        return String.valueOf((char)(string.charAt(0) + shift));
+        if (string == null)
+            throw new NullPointerException("String cannot be null");
+        if (string.isEmpty() || shift == 0) return string;
+        char currentChar = string.charAt(0);
+        if (Character.isAlphabetic(currentChar)) {
+            return String.valueOf((char)((currentChar - SMALL_A + shift) % ALPHA_SIZE + SMALL_A));
+        } else {
+            return String.valueOf(currentChar);
+        }
     }
 }
--- src/test/java/com/xyzcorp/instructor/CaesarShiftTest.java
+++ src/test/java/com/xyzcorp/instructor/CaesarShiftTest.java
@@ -1,8 +1,14 @@
 package com.xyzcorp.instructor;
 
-import org.junit.Test;
-
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.fail;
 import static org.junit.Assert.assertEquals;
+
+import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class CaesarShiftTest {
 
@@ -12,21 +18,78 @@ public class CaesarShiftTest {
 
     @Test
     public void testEncodeWithEmptyStringAndShiftOf0() {
-        String result = CaesarShift.encode("", 0);
-        assertEquals("", result);
+        assertEquals("", CaesarShift.encode("", 0));
     }
     
     @Test
     public void testEncodeWithSmallAAndShiftOf0() {
-    	String result = CaesarShift.encode("a", 0);
-    	assertEquals("a", result);
+    	assertEquals("a", CaesarShift.encode("a", 0));
     }
 
     @Test
     public void testEncodeWithSmallAAndShiftOf1() {
-        String result = CaesarShift.encode("a", 1);
-        assertEquals("b", result);
+        assertThat(CaesarShift.encode("a", 1)).isEqualTo("b");
+    }
+    
+    @Test
+    public void testEncodeWithNullAsAStringClassicWay() {
+    	try {
+    		CaesarShift.encode(null, 0);
+    		fail("This line should not run");
+    	} catch (NullPointerException npe) {
+    		assertEquals("String cannot be null", npe.getMessage());
+    	}
+    }
+    
+    /**
+     * Do not do it this way
+     */
+    @Test(expected = NullPointerException.class)
+    public void testEncodeWithNullAsAStringUsingExpectedAnnotation() {
+    	CaesarShift.encode(null, 0);
     }
 
-    //test for shift of 0
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Test
+    public void testEncodeWithNullAsAStringUsingRule() {
+        expectedException.expectMessage("String cannot be null");
+        expectedException.expect(NullPointerException.class);
+        CaesarShift.encode(null, 0);
+    }
+
+    @Test
+    public void testEncodeWithNullAsAStringUsingAssertJLambdas() {
+        assertThatThrownBy(() -> CaesarShift.encode(null, 0))
+                .hasMessage("String cannot be null")
+                .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    public void testSpecialCharactersWithExclamationMark() {
+        assertThat(CaesarShift.encode("!", 1)).isEqualTo("!");
+    }
+    
+    //Green bar
+    @Test
+    public void testSpecialCharactersWithAtSign() {
+        assertThat(CaesarShift.encode("@", 1)).isEqualTo("@");
+    }
+
+    @Test
+    public void testEncodeWithASmallLetterWithAShiftOf1() {
+        assertThat(CaesarShift.encode("z", 1)).isEqualTo("a");
+    }
+
+    // 'he' -> + 1 -> if
+    // Greek Letters or anything else that is a letter.
+    // Capital letter
+
+
+    //I don't know to do about Greek Letters...
+//    @Test
+//    public void testSpecialCharactersWithAUnicodeThatIsNotStandardAscii() {
+//        assertThat(CaesarShift.encode("Δ", 1)).isEqualTo("Δ");
+//    }
 }
