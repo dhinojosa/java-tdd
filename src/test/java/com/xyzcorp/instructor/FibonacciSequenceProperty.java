dc667d4d278ca3eaa541e24f233d4d8782e6ed19
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2020-05-04 22:09:37 -0600

    added sample files

---
+++ src/test/java/com/xyzcorp/instructor/FibonacciSequenceProperty.java
@@ -0,0 +1,31 @@
+package com.xyzcorp.instructor;
+
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Label;
+import net.jqwik.api.Property;
+import net.jqwik.api.constraints.IntRange;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Label("Fibonacci Sequence Properties")
+public class FibonacciSequenceProperty {
+
+    static Logger logger = LoggerFactory.getLogger(FibonacciSequenceProperty.class);
+
+    @Property
+    @Label("F(n) > n")
+    public void resultShouldBeGreaterThanTheNumberThatItStarted
+        (@ForAll @IntRange(min = 6, max = 34) int i) {
+        assertThat(Fibonacci.apply(i)).isGreaterThan(i);
+    }
+
+    @Property
+    @Label("F(n) = F(n-1) + F(n-2)")
+    public void resultShouldBeSumFibMinus1FibMinus2
+        (@ForAll @IntRange(min = 2, max = 34) int i) {
+        logger.debug(Integer.toString(i));
+        assertThat(Fibonacci.apply(i)).isEqualTo(Fibonacci.apply(i-1) + Fibonacci.apply(i - 2));
+    }
+}
---
+++ src/test/java/com/xyzcorp/instructor/FibonacciSequenceTest.java
@@ -0,0 +1,33 @@
+package com.xyzcorp.instructor;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FibonacciSequenceTest {
+
+
+    //f(n) -> 0 -> if n = 0
+    //     -> 1 -> if n = 1
+    //     -> F(n-1) + F(n - 2) if n > 1
+
+    @Test
+    public void testZero() {
+        assertThat(Fibonacci.apply(0)).isEqualTo(0);
+    }
+
+    @Test
+    public void testOne() {
+        assertThat(Fibonacci.apply(1)).isEqualTo(1);
+    }
+
+    @Test
+    public void testTwo() {
+        assertThat(Fibonacci.apply(2)).isEqualTo(Fibonacci.apply(1) + Fibonacci.apply(0));
+    }
+
+    @Test
+    public void test34() {
+        assertThat(Fibonacci.apply(34)).isEqualTo(5702887);
+    }
+}
