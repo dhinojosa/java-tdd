c6e92ef85fd42e9369f759d65c2393a340857b58
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2020-09-02 08:46:49 -0600

    Removed Files Reset

--- src/main/java/com/xyzcorp/instructor/FindAllStudentsRegisteredService.java
+++
@@ -1,8 +0,0 @@
-package com.xyzcorp.instructor;
-
-import java.util.List;
-import java.util.Map;
-
-public interface FindAllStudentsRegisteredService {
-    Map<Character, List<Student>> groupByFirstLetterOfLastName();
-}
--- src/main/java/com/xyzcorp/instructor/StudentService.java
+++
@@ -1,5 +0,0 @@
-package com.xyzcorp.instructor;
-
-public interface StudentService {
-    boolean registerStudent(Student student);
-}
