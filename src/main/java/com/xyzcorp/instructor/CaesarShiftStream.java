0ba01b25b3dce3563321077c1be36ef3750f4716
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2021-03-30 12:44:37 -0600

    Removed CaesarShift.java to reset, and moved pom.xml to 1.8 so everyone can run it and not have issues.

--- src/main/java/com/xyzcorp/instructor/CaesarShiftStream.java
+++ src/main/java/com/xyzcorp/instructor/CaesarShiftStream.java
@@ -2,8 +2,8 @@ package com.xyzcorp.instructor;
 
 import java.util.Objects;
 
-public interface CaesarShift {
-    static String encode(String string, int shift) {
+public class CaesarShiftStream implements CaesarShift {
+    String encode(String string, int shift) {
         Objects.requireNonNull(string, "String must not be null");
         if (string.isEmpty()) return string;
         return String.valueOf((char) (string.charAt(0) + shift));
